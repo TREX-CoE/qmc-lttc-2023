c7c340dc3501ec16c837137bbea2c2165e46afa4
v1j4y <vijay.gopal.c@gmail.com>
2023-01-19 15:30:28 +0100

    Improved uniform sampling.

--- qmc_uniform.py
+++ qmc_uniform.py
@@ -2,13 +2,28 @@
 
 from hydrogen  import *
 from qmc_stats import *
+import numpy as np
 
 def MonteCarlo(a, nmax):
      energy = 0.
      normalization = 0.
 
      for istep in range(nmax):
-          r = np.random.uniform(-5., 5., (3))
+          #r = np.random.uniform(-5., 5., (3))
+          R = 5.
+          phi = np.random.rand()*2*np.pi
+          costheta = np.random.rand()*2 - 1.0
+          u = np.random.rand()
+
+          theta = np.arccos( costheta )
+          r = R * np.cbrt( u )
+
+          # Spherical distribution
+          x = r * np.sin( theta) * np.cos( phi )
+          y = r * np.sin( theta) * np.sin( phi )
+          z = r * np.cos( theta )
+          r = np.array([x,y,z],dtype=np.Float64)
+
 
           w = psi(a,r)
           w = w*w
